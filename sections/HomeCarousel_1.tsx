4407d1e9849d1186ea6a74d64221c19cc2ad25e1
sopanha.uonmuon <62048484+sopanha-uonmuon@users.noreply.github.com>
2024-12-10 16:32:03 +0700

    mobile

--- sections/HomeCarousel_1.tsx
+++ sections/HomeCarousel_1.tsx
@@ -5,11 +5,11 @@ import Link from 'next/link'
 
 const HomeCarousel_1 = () => {
   return (
-    <div className='w-full section-h flex flex-col justify-start gap-5'>
+    <div className='w-full max-[375px]:w-[75%] section-h flex flex-col justify-start gap-5 max-[375px]:margin-center'>
         <div className='flex justify-between siemreap'>
-            <h1 className='text-xl font-bold ml-20 self-start'>វគ្គសិស្សាថ្មី</h1>
-            <Link href={'/new_course'}>
-            <p className='text-l font-extralight mr-20 underline'>មើលបន្ថែម</p>
+            <h1 className='text-xl max-[375px]:text-base font-bold sm:ml-20 self-start'>វគ្គសិស្សាថ្មី</h1>
+            <Link className='text-l max-[375px]:text-sm font-extralight sm:mr-20 underline' href={'/new_course'}>
+              <p>មើលបន្ថែម</p>
             </Link>
            
         </div>
